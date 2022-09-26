e6ad997a823ba1e1526aec18d9a2fedb5cb33116
arif <muhdarifrawi@gmail.com>
2022-09-26 12:09:50 +0800

    created package file.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// let plaintext = "BGDKKN VNQKC"
-let plaintext = "FC/GGJ RJMG."
+let plaintext = "BGDKKN VNQKC"
+// let plaintext = "FC/GGJ RJMG."
 
 let char = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
             "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
