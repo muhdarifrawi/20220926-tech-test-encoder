a776297d5d5cf433ac05644df8e6afda7c2c7c4e
arif <muhdarifrawi@gmail.com>
2022-09-26 14:02:37 +0800

    made decoder into a module.

---
+++ decoder.js
@@ -0,0 +1,56 @@
+const { useCssModule } = require("vue");
+
+// let plaintext = "BGDKKN VNQKC"
+// let plaintext = "FC/GGJ RJMG."
+
+let char = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+            "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "(", ")", "*", "+", ",", "-", ".",
+            "/"]
+
+let newChar = []
+
+function startDecode(plaintext){
+    createOffset(plaintext)
+}
+
+function createOffset(plaintext) {
+    let offsetVal = checkOffsetVal(plaintext);
+    let setOne = char.slice(-offsetVal);
+    let setTwo = char.slice(0, char.length - offsetVal);
+    newChar = setOne.concat(setTwo);
+    decodeMessage(plaintext);
+}
+
+function checkOffsetVal(plaintext) {
+    let textSplit = plaintext.split(" ");
+    let offsetChar = textSplit[0][0]
+    let searchOffsetVal = char.indexOf(offsetChar);
+    return searchOffsetVal
+}
+
+function decodeMessage(plaintext) {
+    let prepPlaintext = plaintext.slice(1).split("");
+
+    let translateToIndex = prepPlaintext.map(c => {
+        return newChar.indexOf(c)
+    })
+
+    let decode = translateToIndex.map(i => {
+        if(i == -1){
+            return " "
+        }
+        else {
+            return char[i]
+        }
+    })
+
+    console.log(decode.join(""));
+    return decode
+}
+
+
+module.exports = {
+    startDecode
+}
+
--- index.js
+++ index.js
@@ -1,47 +1,4 @@
-let plaintext = "BGDKKN VNQKC"
-// let plaintext = "FC/GGJ RJMG."
-
-let char = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-            "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "(", ")", "*", "+", ",", "-", ".",
-            "/"]
-
-let newChar = []
-
-createOffset();
-
-function createOffset() {
-    let offsetVal = checkOffsetVal(plaintext);
-    let setOne = char.slice(-offsetVal);
-    let setTwo = char.slice(0, char.length - offsetVal);
-    newChar = setOne.concat(setTwo);
-    decodeMessage();
-}
-
-function checkOffsetVal(plaintext) {
-    let textSplit = plaintext.split(" ");
-    let offsetChar = textSplit[0][0]
-    let searchOffsetVal = char.indexOf(offsetChar);
-    return searchOffsetVal
-}
-
-function decodeMessage() {
-    let prepPlaintext = plaintext.slice(1).split("");
-
-    let translateToIndex = prepPlaintext.map(c => {
-        return newChar.indexOf(c)
-    })
-
-    let decode = translateToIndex.map(i => {
-        if(i == -1){
-            return " "
-        }
-        else {
-            return char[i]
-        }
-    })
-
-    console.log(decode.join(""));
-}
+let decoder = require("./decoder")
 
 
+console.log(decoder.startDecode("BGDKKN VNQKC"));
